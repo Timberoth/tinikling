e6b83e5448c8f44d05bf44fe03d5213069384db3
consciousgamer@gmail.com <consciousgamer@gmail.com@26cd38a0-0b11-9761-090f-4fcda7bee94b>
2011-09-25 20:52:04 +0000

    Working looping on FootPattern1.

--- trunk/Assets/Scripts/Feet/FootPatternEvent.cs
+++ trunk/Assets/Scripts/Feet/FootPatternEvent.cs
@@ -3,16 +3,22 @@ using System.Collections;
 public class FootPatternEvent {
 	
 	// Name of FootSymbol object
-	public string symbolName;
+	public string symbolName = "";
 	
 	// Time of event
-	public float time;
+	public float time = 0.0f;
 	
 	// Foot State - Up, Down, Inactive
-	public FootSymbol.FootState state;
+	public FootSymbol.FootState state = FootSymbol.FootState.Inactive;
 	
 	// Foot - Left, Right
-	public FootSymbol.Foot foot;
+	public FootSymbol.Foot foot = FootSymbol.Foot.Left;
 	
-	public bool flipped;
+	// Is foot direction reversed
+	public bool flipped = false;
+	
+	// True if this event should only be fired once and not repeated
+	// in the second cyle.  
+	// This is mainly used for the starting feet positions.
+	public bool oneShot = false;
 }
