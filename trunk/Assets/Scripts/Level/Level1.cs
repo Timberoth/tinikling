a45b790d2372dbdfaf03c04aeee503392c350b92
consciousgamer@gmail.com <consciousgamer@gmail.com@26cd38a0-0b11-9761-090f-4fcda7bee94b>
2011-11-03 07:48:39 +0000

    Added music and changing foot pattern and stick speed.

--- trunk/Assets/Scripts/Level/Level1.cs
+++ trunk/Assets/Scripts/Level/Level1.cs
@@ -7,8 +7,8 @@ public class Level1 : Level {
 	{		
 		LevelEvent levelEvent = new LevelEvent();
 		levelEvent.triggerTime = 2.0f;
-		levelEvent.footPatternSpeed = 2.0f;
-		levelEvent.stickTempoSpeed = 2.0f;
+		levelEvent.footPatternSpeed = 1.5f;
+		levelEvent.stickTempoSpeed = 1.5f;
 		levelEvent.callbackObject = this.gameObject;
 		levelEvent.callbackMessage = "Level1SpecificFunction";
 		levelEvent.callbackData = "Callback is working for Level 1";
@@ -16,9 +16,9 @@ public class Level1 : Level {
 		
 		
 		levelEvent = new LevelEvent();
-		levelEvent.triggerTime = 5.0f;
-		levelEvent.footPatternSpeed = 1.0f;
-		levelEvent.stickTempoSpeed = 1.0f;
+		levelEvent.triggerTime = 8.0f;
+		levelEvent.footPatternSpeed = 3.0f;
+		levelEvent.stickTempoSpeed = 3.0f;
 		levelEvents.Enqueue( levelEvent );
 	}
 	
